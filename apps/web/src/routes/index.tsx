0793ac71d8591571020fb111be3328ec36db41b9
habibium <habib.prodev@gmail.com>
2026-05-24 15:57:01 +0600

    chore: init shadcn

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '~/components/ui/button'
 
 export const Route = createFileRoute('/')({ component: Home })
 
@@ -9,6 +10,12 @@ function Home() {
       <p className="mt-4 text-lg">
         Edit <code>src/routes/index.tsx</code> to get started.
       </p>
+      <Button>Click</Button>
+      <Button variant="destructive">Click</Button>
+      <Button variant="ghost">Click</Button>
+      <Button variant="link">Click</Button>
+      <Button variant="outline">Click</Button>
+      <Button variant="secondary">Click</Button>
     </div>
   )
 }
