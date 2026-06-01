dfe442d9280b464fc06478d4f12e6fa7a3aad8bd
habibium <habib.prodev@gmail.com>
2026-06-01 12:34:08 +0600

    refactor: update Vite configuration and remove wrangler.jsonc

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,17 +1,23 @@
-import { cloudflare } from '@cloudflare/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react-swc'
+import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
     devtools(),
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindcss(),
-    tanstackStart(),
+    tanstackStart({
+      prerender: {
+        enabled: true,
+        autoSubfolderIndex: true,
+        autoStaticPathsDiscovery: true,
+        crawlLinks: true,
+        failOnError: true,
+      },
+    }),
     viteReact(),
   ],
 })
