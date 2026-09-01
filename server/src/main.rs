289aefb866ad23c8113a1687b2c748fa7279b35f
habibium <habib.prodev@gmail.com>
2026-09-01 16:50:31 +0600

    chore: setup hello world server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+const PORT: u16 = 8000;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new()
+        .route("/api/hello", get(|| async { "Hello, World!" }))
+        .layer(TraceLayer::new_for_http());
+
+    let Ok(listener) = TcpListener::bind(format!("0.0.0.0:{PORT}")).await else {
+        tracing::error!("Failed to bind to port {}", PORT);
+        std::process::exit(1);
+    };
+
+    axum::serve(listener, app).await.unwrap();
 }
