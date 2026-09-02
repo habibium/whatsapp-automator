48fd2f155007a2b5c95d6d6288a8353a520067e6
habibium <habib.prodev@gmail.com>
2026-09-02 17:51:08 +0600

    chore: update configuration files and improve type imports

--- web/src/api/client.ts
+++ web/src/api/client.ts
@@ -1,6 +1,6 @@
 import createClient from 'openapi-fetch'
 import createQueryClient from 'openapi-react-query'
-import type { paths } from './schema.d.ts'
+import type { paths } from './schema'
 
 export const api = createClient<paths>()
 export const $api = createQueryClient(api)
